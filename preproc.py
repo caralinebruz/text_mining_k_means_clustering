014a0f59af670bceef0ac89490f2ccb70791076d
caralinebruz <caraline.bruzinski@gmail.com>
2022-07-11 17:23:13 -0400

    initial commit

---
+++ preproc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+
+files = [
+"data/C1/article01.txt",
+"data/C1/article02.txt",
+"data/C1/article03.txt",
+"data/C1/article04.txt",
+"data/C1/article05.txt",
+"data/C1/article06.txt",
+"data/C1/article07.txt",
+"data/C1/article08.txt"
+]
+
+# preprocess the raw data
+
+def do_stuff():
+
+	print("hello\n");
+
+	for file in files:
+		print(file + "\n");
+
+
+if __name__ == '__main__':
+	do_stuff()
