0fbacfc0657b071769e821379cd10f8acef35ab9
caralinebruz <caraline.bruzinski@gmail.com>
2022-07-14 14:34:26 -0400

    working matrix version

--- preproc.py
+++ preproc.py
@@ -16,9 +16,8 @@ from nltk.metrics import BigramAssocMeasures
 
 import spacy
 from spacy import displacy
-# en-core-web-sm-3.4.0
-# python -m spacy download en_core_web_sm
 
+import itertools
 
 
 
@@ -43,14 +42,17 @@ files = [
 	"C1/article08.txt"
 ]
 
+my_process_objects = []
 
 
-# class Matrix():
-# 	def __init__(self):
-# 		preprocessed_file_objects = []
 
 
 class Preprocessor():
+
+	# easy way to increment an index of the class instance
+	# https://stackoverflow.com/questions/1045344/how-do-you-create-an-incremental-id-in-a-python-class
+	index = itertools.count()
+
 	def __init__(self, file):
 		self.filename = "./" + infile_path + file
 		self.out_filename = "./" + outfile_path + file
@@ -62,9 +64,7 @@ class Preprocessor():
 		self.keywords_concepts = []
 		self.ngrams = []
 		self.ngrams_frequency = {}
-
-
-
+		self.index = next(Preprocessor.index)
 
 	def read_file(self):
 		# read the file into an array of lines
@@ -74,7 +74,6 @@ class Preprocessor():
 			self.lines = f.readlines()
 
 			logger.info("line 0: %s", self.lines[0])
-
 
 	def filter_stopwords_lemmatize(self):
 		logger.info("removing stopwords ...")
@@ -117,7 +116,6 @@ class Preprocessor():
 
 		self.document_text = ' '.join(self.document)
 
-
 	def apply_ner(self):
 		logger.info("applying NER ...")
 		NER = spacy.load('en_core_web_sm')
@@ -154,7 +152,6 @@ class Preprocessor():
 			bigram_string = ' '.join(gram)
 			self.ngrams.append(bigram_string)
 
-
 	def _find_tri_grams(self, text):
 		# this doesnt seem to be producing as meaningful result as the bigram :/
 
@@ -163,7 +160,6 @@ class Preprocessor():
 
 			trigram_string = ' '.join(gram)
 			self.ngrams.append(trigram_string)
-
 
 	def sliding_window_merge(self):
 		logger.info("using a sliding window to merge remaining phrases ...")
@@ -202,8 +198,8 @@ class Preprocessor():
 		for ngram in self.ngrams:
 			frequency = self.document_text.count(ngram)
 		
-			self.ngrams_frequency['ngram'] = frequency
-			# print("%s : %s "% (ngram, frequency))
+			self.ngrams_frequency[ngram] = frequency
+			#print("%s : %s "% (ngram, frequency))
 
 			# if frequency > 1, merge
 			if frequency > 1:
@@ -214,17 +210,33 @@ class Preprocessor():
 
 				# then also replace the original text document
 				self.document_text = self.document_text.replace(ngram, new_ngram)	
-				
 
+				print("\t\t %s : %s "% (ngram, frequency))
+
+
+	def cleanup(self):
+
+		for i in range(len(self.keywords_concepts)):
+			self.keywords_concepts[i] = self.keywords_concepts[i].replace("_"," ").lower()
+			# print(self.keywords_concepts[i])
+
+		self.ngrams_frequency = {k.replace("_"," ").lower() : v for k, v in self.ngrams_frequency.items()}
+		# print(self.ngrams_frequency.items())
+
+		self.document_text = self.document_text.replace("_"," ").lower()
+		# print(self.document_text)
+
+				
 	def write_output(self):
 
 		logger.info("Writing output file "  + self.out_filename);
 
+		# WRITE THIS FILE WITHOUT ANY UNDERSCORES
+
 		with open(self.out_filename, "w") as outfile:
 			for word in self.document_text:
+
 				outfile.write(word.lower())
-
-
 
 
 def write_keywords_concepts_file(P):
@@ -236,7 +248,7 @@ def write_keywords_concepts_file(P):
 
 		lines = P.keywords_concepts
 		for line in lines:
-			print(line)
+			# print(line)
 			f.write(line)
 			f.write("\n")
 
@@ -245,15 +257,13 @@ def write_keywords_concepts_file(P):
 # preprocess the raw data
 def do_preprocessing():
 
-	#for file in files[0:2]:
-	for file in files:
+	for file in files[0:3]:
+	#for file in files:
 
-		# get the fullpath together
-		# filename = "./" + infile_path + file
-		# logger.info("Starting with file "  + filename);
-
-		# now, instantiate a preprocess object
 		P = Preprocessor(file)
+
+		# and add that object to the processed objects list
+		my_process_objects.append(P)
 
 		# read the file
 		P.read_file()
@@ -262,29 +272,116 @@ def do_preprocessing():
 		# https://www.geeksforgeeks.org/python-lemmatization-with-nltk/
 		P.filter_stopwords_lemmatize()
 
-
 		# 3 - apply NER 
 		# https://www.analyticsvidhya.com/blog/2021/06/nlp-application-named-entity-recognition-ner-in-python-with-spacy/#:~:text=Named%20Entity%20Recognition%20is%20the,%2C%20money%2C%20time%2C%20etc.
 		P.apply_ner()
 
-
 		# 4 - use sliding window approach to merge remaining phrases
 		P.sliding_window_merge()
 
-		print(P.document_text)
+		# clean up my findings:
+		# 	removes underscores from document text, lowercases
+		#	removes underscores from frequency keys, lowercases
+		P.cleanup()
 
-		# 5 - at the end, write to out_file for each document
+		# 5 - at the end, write to out_file for each document for safety
 		P.write_output()
 
 		# also write the keywords concepts file
 		write_keywords_concepts_file(P)
 
-
+	return my_process_objects
 
 
 
 # # collect keywords and terms across all the files
 # def generate_term_document_matrix():
+class DocuTermMatrix():
+
+	def __init__(self):
+		self.keywords_concepts = []
+		self.matrix = []
+
+	def consolidate_keywords_concepts(self):
+		# read the file into an array of lines
+		logger.info("Collecting all of the keywords concepts ...")
+		for file_object in my_process_objects:
+
+			for keyword in file_object.keywords_concepts:
+				if keyword not in self.keywords_concepts:
+
+					self.keywords_concepts.append(keyword.lower())
+
+
+	def initialize_matrix(self):
+		logger.info("initializing the zero matrix ...")
+
+		# fill with 0s for the correct size matrix
+		num_rows = len(my_process_objects)
+		num_cols = len(self.keywords_concepts)
+
+		# https://intellipaat.com/community/63426/how-to-create-a-zero-matrix-without-using-numpy
+		self.matrix = [([0]*num_cols) for i in range(num_rows)]
+
+
+	def fill_matrix(self):
+
+		logger.info("Creating the document term matrix ...")
+		i = 0
+		for i in range(len(my_process_objects)):
+
+			# print(i)
+			# print(my_process_objects[i].index)
+			# print(my_process_objects[i].filename)
+
+			file_object = my_process_objects[i]
+
+			# convert all the keys to lowercase for now
+			the_files_ngrams =  {k.lower(): v for k, v in file_object.ngrams_frequency.items()}
+			# print(file_object.ngrams_frequency)
+
+
+			# iterate over the keywords_concepts list
+			for j in range(len(self.keywords_concepts)):
+
+				# if a keyword_concept is in the document_text of the document
+				# count the number of times the substring appears
+				if self.keywords_concepts[j] in file_object.document_text:
+
+					# print("\n TRUE !")
+					# https://stackoverflow.com/questions/8899905/count-number-of-occurrences-of-a-substring-in-a-string
+					frequency = file_object.document_text.count(self.keywords_concepts[j])
+					self.matrix[i][j] = frequency
+
+				# else:
+				# 	print("%s not in document text" % self.keywords_concepts[j])
+				# 	print(file_object.document_text)
+
+
+
+
+def generate_document_term_matrix():
+
+	M = DocuTermMatrix()
+
+	# firt, consolidate and dedupe all keywords across the files
+	M.consolidate_keywords_concepts()
+
+	# second, create the matrix
+	#	rows will be filenames (file_object.index)
+	#	columns will be keywords (keywords_concepts)
+	#	populate cell value based on the FREQUENCY of that keyword in the file
+
+
+	print(M.keywords_concepts)
+
+	# second, create the matrix
+	M.initialize_matrix()
+	M.fill_matrix()
+
+	# print(M.matrix)
+	for k in range(len(M.matrix)):
+		print(M.matrix[k])
 
 
 
@@ -293,14 +390,58 @@ def do_preprocessing():
 if __name__ == '__main__':
 
 	logger.info("starting ...");
+	# global file_object_index
+	# file_object_index = 0
 
-	# # for the collection of metadata
-	# M = Matrix()
-
-	do_preprocessing()
+	# does preprocessing on the files
+	# returns a list of preprocessed file objects for each file
+	logger.info("First: Do preprocessing on the files")
+	processed_objects = do_preprocessing()
 
 	# then give it the matrix class here
-	# generate_term_document_matrix(M)
+	logger.info("Next: Generating Document Term Matrix")
+	generate_document_term_matrix()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
